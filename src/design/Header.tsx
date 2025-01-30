dfa223dcaf97b1c268a752adb567d2e284737e9c
Matthintosh <rocher.matthieu@gmail.com>
2025-01-30 10:12:03 +0100

    refactor: Add prettier for code refactoring.

--- src/design/Header.tsx
+++ src/design/Header.tsx
@@ -10,5 +10,7 @@ export const Header = styled.div`
   left: 0;
   display: flex;
   align-items: center;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+  box-shadow:
+    0 2px 4px 0 rgba(0, 0, 0, 0.2),
+    0 3px 10px 0 rgba(0, 0, 0, 0.19);
 `;
