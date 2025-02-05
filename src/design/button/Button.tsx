bc9dfc291dfc34bb1040081894b304930c308110
Matthintosh <rocher.matthieu@gmail.com>
2025-02-05 10:01:41 +0100

    ui: Add designed button

---
+++ src/design/button/Button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Button = styled.button<{ $loading?: boolean }>`
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: #d9d5c3;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${(props) =>
+    props.$loading &&
+    `
+    background-color: #EFEFEF;
+    &::before {
+    pointer-events: none;
+    content: "";
+    display: inline-block;
+    margin-right: 0.5rem;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    border: 2px solid currentColor;
+    border-top-color: transparent;
+    animation: spinner 1s linear infinite;
+  }
+  @keyframes spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  `}
+`;
